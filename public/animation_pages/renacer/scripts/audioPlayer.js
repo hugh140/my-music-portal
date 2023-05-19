fc91633eb6d1284a3a17e264fac9355dfaa40b82
hugh140 <hugofer300@gmail.com>
2023-05-18 22:03:46 -0500

    First deploy and first animation finished

---
+++ public/animation_pages/renacer/scripts/audioPlayer.js
@@ -0,0 +1,46 @@
+const playIcon =
+  '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><!--! Font Awesome Pro 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M73 39c-14.8-9.1-33.4-9.4-48.5-.9S0 62.6 0 80V432c0 17.4 9.4 33.4 24.5 41.9s33.7 8.1 48.5-.9L361 297c14.3-8.7 23-24.2 23-41s-8.7-32.2-23-41L73 39z"/></svg>';
+
+const pauseIcon =
+  '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><!--! Font Awesome Pro 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M48 64C21.5 64 0 85.5 0 112V400c0 26.5 21.5 48 48 48H80c26.5 0 48-21.5 48-48V112c0-26.5-21.5-48-48-48H48zm192 0c-26.5 0-48 21.5-48 48V400c0 26.5 21.5 48 48 48h32c26.5 0 48-21.5 48-48V112c0-26.5-21.5-48-48-48H240z"/></svg>';
+
+function playMusic() {
+  Tone.loaded().then(() => {
+    if (isPlaying) {
+      kick.stop();
+      drums.stop();
+      hihats.stop();
+      ambient.stop();
+      ambient2.stop();
+      finishedTime = Tone.immediate();
+      currentTime += finishedTime - initialMusicTime;
+      playPauseButton.innerHTML = playIcon;
+    } else {
+      kick.start().seek(initialMusicTime);
+      drums.start().seek(initialMusicTime);
+      hihats.start().seek(initialMusicTime);
+      ambient.start().seek(initialMusicTime);
+      ambient2.start().seek(initialMusicTime);
+
+      initialMusicTime = Tone.immediate();
+      playPauseButton.innerHTML = pauseIcon;
+    }
+
+    isPlaying = !isPlaying;
+  });
+}
+
+function stopMusic() {
+  Tone.loaded().then(() => {
+    kick.stop();
+    drums.stop();
+    hihats.stop();
+    ambient.stop();
+    ambient2.stop();
+    currentTime = 0;
+    initialMusicTime = 0;
+    finishedTime = 0;
+    isPlaying = false;
+    playPauseButton.innerHTML = playIcon;
+  });
+}
