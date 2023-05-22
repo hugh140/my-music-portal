1536b4e11bc79c5051096921b1e511b2e5927e31
hugh140 <hugofer300@gmail.com>
2023-05-22 14:39:45 -0500

    Finished the second animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 function App() {
-
   return (
     <>
       <div className="grid place-content-center h-screen">
         <a href="renacer.html">
           <button className="border-2 p-2">#1</button>
         </a>
-        <button className="border-2 p-2">#2</button>
+        <a href="eter.html">
+          <button className="border-2 p-2">#2</button>
+        </a>
       </div>
     </>
   );
