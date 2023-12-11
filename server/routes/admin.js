463b10f75f678ead8f69ffad9c3444d7ec425b78
hugh140 <hugofer300@gmail.com>
2023-12-10 21:53:03 -0500

    Fixing deploy errors

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -79,7 +79,7 @@ router.post("/login", async (req, res) => {
         secure: true,
         maxAge: 1000 * 60 * 60,
         sameSite: "none",
-        httpOnly: false,
+        domain: ".vercel.app",
       });
       res.json({ message: "You are logged successfully.", ok: true });
     } else
