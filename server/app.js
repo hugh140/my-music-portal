d0abdda0d8fd0eed7ed331aedc3616393b4b35b2
hugh140 <hugofer300@gmail.com>
2023-12-10 20:54:21 -0500

    Fixing deploy errors

--- server/app.js
+++ server/app.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(
   cors({
-    origin: ["https://my-music-portal.vercel.app"],
+    origin: ["http://localhost:5173"],
     credentials: true,
   })
 );
