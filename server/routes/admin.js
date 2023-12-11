915c6baab319533cefb7694154bd6a60c7ea4d59
hugh140 <hugofer300@gmail.com>
2023-12-10 21:45:49 -0500

    fixing deploy errors

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -78,6 +78,7 @@ router.post("/login", async (req, res) => {
       res.cookie("HR", token, {
         secure: true,
         maxAge: 1000 * 60 * 60,
+        sameSite: "none",
         httpOnly: false,
       });
       res.json({ message: "You are logged successfully.", ok: true });
